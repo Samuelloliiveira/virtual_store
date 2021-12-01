70b5e54d9efb5ec6d7a1c21a69473a08fec64d12
samuel <samuelcontatodev@gmail.com>
2021-12-01 13:40:10 -0300

    imagem de fundo, mas efeito ao carregar a pagina

---
+++ scripts/scrollReveal.js
@@ -0,0 +1,12 @@
+const scrollReveal = ScrollReveal({
+  
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true,
+  
+  })
+  
+  scrollReveal.reveal(`.card, #search, #info div `, {interval: 100}
+  )
+  
